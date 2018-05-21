bde8e1a91cda0ea92c7b7c2e3dcd591786658bb0
Andrew Morozow <andrey940@gmail.com>
2018-05-21 17:40:30 +0300

    fonts add + popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function openPopup() {
+    var popup = document.getElementById("videoPopup");
+    popup.classList.add("show");
+}
+
+function closePopup() {
+    var popup = document.getElementById("videoPopup");
+    popup.classList.remove("show");
+}
